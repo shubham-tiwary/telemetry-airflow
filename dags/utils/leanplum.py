5ef3fa3b39edcc41cd8a7e72a2f998427b5aebe7
Benjamin Wu <benjaminwu124@gmail.com>
2020-06-25 10:47:59 -0400

    Add task for leanplum streaming export

--- dags/utils/leanplum.py
+++ dags/utils/leanplum.py
@@ -1,16 +1,20 @@
 from airflow.contrib.hooks.gcp_api_base_hook import GoogleCloudBaseHook
 from operators.gcp_container_operator import GKEPodOperator
 
+
 def export(
-    leanplum_app_id,
-    leanplum_client_key,
     bq_dataset_id,
     task_id,
     bq_project,
+    leanplum_app_id=None,
+    leanplum_client_key=None,
+    s3_bucket="moz-fx-data-us-west-2-leanplum-export",
     gcs_bucket="moz-fx-data-prod-external-data",
     table_prefix=None,
     gcs_prefix=None,
     project_id=None,
+    version="1",
+    streaming=False,
     gcp_conn_id="google_cloud_derived_datasets",
     gke_location="us-central1-a",
     gke_cluster_name="bq-load-gke-1",
@@ -24,14 +28,17 @@ def export(
     See bug 1588654 for information on which buckets and datasets
     these tabes should live in.
 
-    :param str leanplum_app_id:      [Required] Leanplum application ID
-    :param str leanplum_client_key:  [Required] Leanplum client key
     :param str bq_dataset:           [Required] BigQuery default dataset id
     :param str task_id:              [Required] The task ID for this task
     :param str bq_project:           [Required] The project to create tables in
+    :param str leanplum_app_id:      [Required for historical export] Leanplum application ID
+    :param str leanplum_client_key:  [Required for historical export] Leanplum client key
+    :param str s3_bucket:            S3 bucket to retrieve streaming exports from
     :param str gcs_bucket:           GCS Bucket to export data to
     :param str gcs_prefix:           Prefix for data exported to GCS
     :param str project_id:           Project the GKE cluster is in
+    :param str version:              Version of the destination table
+    :param bool streaming:           Use Leanplum streaming export
     :param str gcp_conn_id:          Airflow connection id for GCP access
     :param str gke_location:         GKE cluster location
     :param str gke_cluster_name:     GKE cluster name
@@ -49,18 +56,27 @@ def export(
 
     args = ["leanplum-data-export",
             "export-leanplum",
-            "--app-id", leanplum_app_id,
-            "--client-key", leanplum_client_key,
             "--date", "{{ ds_nodash }}",
             "--bucket", gcs_bucket,
             "--bq-dataset", bq_dataset_id,
-            "--project", bq_project]
+            "--project", bq_project,
+            "--s3-bucket", s3_bucket,
+            "--version", version]
+
+    if leanplum_app_id is not None:
+        args += ["--app-id", leanplum_app_id]
+
+    if leanplum_client_key is not None:
+        args += ["--client-key", leanplum_client_key]
 
     if gcs_prefix is not None:
         args += ["--prefix",  gcs_prefix]
 
     if table_prefix is not None:
         args += ["--table-prefix", table_prefix]
+
+    if streaming:
+        args.append("--streaming")
 
     return GKEPodOperator(
         task_id=task_id,
